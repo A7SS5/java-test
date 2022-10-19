5fb430680f0b44135377b2d089ab43c30c63c0c7
A7SS5 <1337903705@qq.com>
2022-10-19 14:12:44 +0800

    test

---
+++ hello.java
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: hello.java
+    > Author: zll
+    > Mail: zhnllion@126.com 
+    > Created Time: 2022年10月19日 星期三 14时07分41秒
+ ************************************************************************/
+public class hello{
+	public static  void  main(String [] args)
+	{
+		System.out.println("hello");
+		
+	}
+}
